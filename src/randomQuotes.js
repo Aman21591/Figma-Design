6ae56742a9c73920b1aaf8c31d4688af0d493e3f
Aman Yadav <61842919+Aman21591@users.noreply.github.com>
2022-01-15 12:55:55 +0530

    Update randomQuotes.js

--- src/randomQuotes.js
+++ src/randomQuotes.js
@@ -4,5 +4,10 @@ export default [
 "Review the facts cool is the best.",
 "There is no Sore it will Not Heal, No cool it will not Subdue.",
 "There is no Sore it will Not Heal, No cool it will not Subdue.",
-"There is no Sore it will Not Heal, No cool it will not Subdue.
+"There is no Sore it will Not Heal, No cool it will not Subdue.",
+"coziness building for tomorrow.",
+"coziness building for tomorrow.",
+"coziness building for tomorrow.",
+"coziness building for tomorrow.",
+"coziness building for tomorrow."
 ];
