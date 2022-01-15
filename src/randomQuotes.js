9f2561cc4dc43bc5f2d8f69fed2db26392e52e27
Aman Yadav <61842919+Aman21591@users.noreply.github.com>
2022-01-15 12:50:36 +0530

    Update randomQuotes.js

--- src/randomQuotes.js
+++ src/randomQuotes.js
@@ -1,14 +1,8 @@
 export default [
-  "Life isn't about getting and having, it's about giving and being.",
-  "Whatever the mind of man can conceive and believe, it can achieve.",
-  "Strive not to be a success, but rather to be of value.",
-  "I attribute my success to this: I never gave or took any excuse.",
-  "You miss 100% of the shots you don't take.",
-  "The most difficult thing is the decision to act, the rest is merely tenacity.",
-  "Every strike brings me closer to the next home run.",
-  "Definiteness of purpose is the starting point of all achievement.",
-  "We must balance conspicuous consumption with conscious capitalism.",
-  "Life is what happens to you while you're busy making other plans.",
-  "We become what we think about.",
-  "The most common way people give up their power is by thinking they don't have any.",
+"There is no Sore it will Not Heal, No cool it will not Subdue.",
+"There is no Sore it will Not Heal, No cool it will not Subdue.",
+"Review the facts cool is the best.",
+"There is no Sore it will Not Heal, No cool it will not Subdue.",
+"There is no Sore it will Not Heal, No cool it will not Subdue.",
+"There is no Sore it will Not Heal, No cool it will not Subdue.
 ];
